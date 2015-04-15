08a97bc07381a9b28dec3662fd2c54c7819e7852
Devin <devin@interviewmaster.in>
2015-04-15 05:21:09 -0400

    first commit

---
+++ TimeStamp/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCopyTS = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtSeparator = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCopyTS
+            // 
+            this.btnCopyTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopyTS.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnCopyTS.Location = new System.Drawing.Point(173, 140);
+            this.btnCopyTS.Name = "btnCopyTS";
+            this.btnCopyTS.Size = new System.Drawing.Size(119, 48);
+            this.btnCopyTS.TabIndex = 0;
+            this.btnCopyTS.Text = "Copy Time Stamp";
+            this.btnCopyTS.UseVisualStyleBackColor = true;
+            this.btnCopyTS.Click += new System.EventHandler(this.btnCopyTS_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMessage.ForeColor = System.Drawing.Color.Moccasin;
+            this.lblMessage.Location = new System.Drawing.Point(41, 34);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(251, 18);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Time Stamp copied to clipboard!";
+            this.lblMessage.Visible = false;
+            // 
+            // txtSeparator
+            // 
+            this.txtSeparator.Location = new System.Drawing.Point(118, 154);
+            this.txtSeparator.MaxLength = 1;
+            this.txtSeparator.Name = "txtSeparator";
+            this.txtSeparator.Size = new System.Drawing.Size(35, 20);
+            this.txtSeparator.TabIndex = 2;
+            this.txtSeparator.Text = "_";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Separator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.SlateGray;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(41, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 34);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Copies Time Stamp to clipboard: \r\nyear_month_day_hrs_min_sec";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Moccasin;
+            this.lblResult.Location = new System.Drawing.Point(41, 189);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 18);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblResult.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(334, 231);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSeparator);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnCopyTS);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Stamp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCopyTS;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtSeparator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ TimeStamp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetTimeStamp();
+        }
+
+        private void GetTimeStamp()
+        {
+            lblMessage.Visible = false;
+            lblResult.Visible = false;
+
+            try
+            {
+                //default separator
+                string Separator = "_";
+
+                if (!string.IsNullOrEmpty(txtSeparator.Text))
+                    Separator = txtSeparator.Text;
+
+                DateTime dt = DateTime.Now;
+
+                string[] TimeStamp = new string[6];
+
+                TimeStamp[0] = dt.Year.ToString();
+                TimeStamp[1] = dt.ToString("MM");
+                TimeStamp[2] = dt.ToString("dd");
+                TimeStamp[3] = dt.ToString("HH");
+                TimeStamp[4] = dt.ToString("mm");
+                TimeStamp[5] = dt.ToString("ss");
+
+                string Result = string.Join(Separator, TimeStamp);
+                Clipboard.SetText(Result);
+
+                lblMessage.Text = "Time Stamp copied to clipboard!";
+                lblResult.Text = Result;
+            }
+            catch
+            {
+                lblMessage.Text = "Error! Please try again";
+                lblResult.Text = "";
+            }
+
+            lblMessage.Visible = true;
+            lblResult.Visible = true;
+        }
+
+        private void btnCopyTS_Click(object sender, EventArgs e)
+        {
+            GetTimeStamp();
+        }
+
+
+    }
+}
